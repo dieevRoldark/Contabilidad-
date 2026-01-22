cf38d3d2f68042a86a144d5d54b810728e73cd6d
Diego <droldan9@estudiantes.areandina.edu.co>
2026-01-21 22:25:23 -0500

    Aplicando JavaScript

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
-//En esta estructura la usar Queryselector solo nos permite 
-//aplicar los eventos a un solo elemento o en este caso un boton.
+/*En esta estructura la usar Queryselector solo nos permite RECUPER O
+aplicar los eventos a un solo elemento o en este caso un boton.
 
 const boton = document.querySelector('.main__header-button')
 
@@ -9,3 +9,33 @@ boton.addEventListener( 'click', function() {
     boton.classList.add("is-applied")
     boton.disabled = true
 })
+
+Tener en cuenta!!! = Para recuperar todos los elementos usamos Queryselectorall
+
+*/
+
+//PreventDefault permite cambiar el comportamiento del evento:input
+
+const searchInput = document.querySelector('#input-search')
+
+searchInput.addEventListener('input', function(event) {
+    event.preventDefault()
+    console.log(searchInput.value)
+})
+
+
+//Capturar evento:click dentro del contenedor mediante event 
+//Condicional que captura los botones del contenedor y los cuenta + estilos
+
+const mainSection = document.querySelector('.main__header')
+
+mainSection.addEventListener('click', function(event) {
+    const element = event.target
+
+    if (element.classList.contains('main__header-button')) {
+        element.classList.add("is-applied")
+        element.disabled = true
+    }
+})
+
+
