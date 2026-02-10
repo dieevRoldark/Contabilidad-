ac79cb3e6427a4428892b85b0e1cc44f0abd6443
Diego <droldan9@estudiantes.areandina.edu.co>
2026-02-10 12:55:35 -0500

    Aplicando la logica de los modulos cliente y proveedores para recuperar informacion de tabla y evento de botones

---
+++ controller/clientes.js
@@ -0,0 +1,45 @@
+export function init() {
+
+    const btnNuevoCliente = document.getElementById('btn-cliente');
+
+    if (btnNuevoCliente) {
+        btnNuevoCliente.addEventListener ('click', () => alert('Agregar nuevo cliente'));
+    }
+
+    cargarTablaClientes();
+}
+
+
+function cargarTablaClientes() {
+    const dataClientes = [
+        { cedula: '1234567890', nombre: 'Juan Pérez', telefono: '0987654321', correo: 'client@example', direccion: 'Av. Siempre Viva 123' },
+        { cedula: '1234567890', nombre: 'Juan Pérez', telefono: '0987654321', correo: 'client@example', direccion: 'Av. Siempre Viva 123' },
+        { cedula: '1234567890', nombre: 'Juan Pérez', telefono: '0987654321', correo: 'client@example', direccion: 'Av. Siempre Viva 123' },
+        { cedula: '1234567890', nombre: 'Juan Pérez', telefono: '0987654321', correo: 'client@example', direccion: 'Av. Siempre Viva 123' },
+        { cedula: '1234567890', nombre: 'Juan Pérez', telefono: '0987654321', correo: 'client@example', direccion: 'Av. Siempre Viva 123' },
+        { cedula: '1234567890', nombre: 'Juan Pérez', telefono: '0987654321', correo: 'client@example', direccion: 'Av. Siempre Viva 123' },
+        { cedula: '1234567890', nombre: 'Juan Pérez', telefono: '0987654321', correo: 'client@example', direccion: 'Av. Siempre Viva 123' },
+        { cedula: '1234567890', nombre: 'Juan Pérez', telefono: '0987654321', correo: 'client@example', direccion: 'Av. Siempre Viva 123' },
+        { cedula: '1234567890', nombre: 'Juan Pérez', telefono: '0987654321', correo: 'client@example', direccion: 'Av. Siempre Viva 123' },
+        { cedula: '1234567890', nombre: 'Juan Pérez', telefono: '0987654321', correo: 'client@example', direccion: 'Av. Siempre Viva 123' },
+        { cedula: '1234567890', nombre: 'Juan Pérez', telefono: '0987654321', correo: 'client@example', direccion: 'Av. Siempre Viva 123' },
+    ]
+
+    const tbody = document.querySelector('#tbodyClientes tbody')
+    
+    tbody.innerHTML = ''
+    
+    dataClientes.forEach(cliente => {
+        const fila = `
+            <tr>
+                <td>${cliente.cedula}</td>
+                <td>${cliente.nombre}</td>
+                <td>${cliente.telefono}</td>
+                <td>${cliente.correo}</td>
+                <td>${cliente.direccion}</td>
+            </tr>
+        `
+
+        tbody.innerHTML += fila
+    })
+}
---
+++ controller/proveedores.js
@@ -0,0 +1,44 @@
+export function init() {
+
+    const btnNuevoProveedor = document.getElementById('btn-Proveedores');
+
+    if (btnNuevoProveedor) {
+        btnNuevoProveedor.addEventListener ('click', () => alert('Agregar nuevo proveedor'));
+    }
+
+    cargarTablaProveedores();
+}
+
+
+function cargarTablaProveedores() {
+    const dataProveedores = [
+        { nit: 1, nombre: 'SuperOriente', telefono: '123456789', direccion: 'Calle 123', email: 'Superori@example.com'},
+        { nit: 2, nombre: 'SuperOccidente', telefono: '987654321', direccion: 'Avenida 456', email: 'nuevoprove@example.com'},
+        { nit: 2, nombre: 'SuperOccidente', telefono: '987654321', direccion: 'Avenida 456', email: 'nuevoprove@example.com'},
+        { nit: 2, nombre: 'SuperOccidente', telefono: '987654321', direccion: 'Avenida 456', email: 'nuevoprove@example.com'},
+        { nit: 2, nombre: 'SuperOccidente', telefono: '987654321', direccion: 'Avenida 456', email: 'nuevoprove@example.com'},
+        { nit: 2, nombre: 'SuperOccidente', telefono: '987654321', direccion: 'Avenida 456', email: 'nuevoprove@example.com'},
+        { nit: 2, nombre: 'SuperOccidente', telefono: '987654321', direccion: 'Avenida 456', email: 'nuevoprove@example.com'},
+        { nit: 2, nombre: 'SuperOccidente', telefono: '987654321', direccion: 'Avenida 456', email: 'nuevoprove@example.com'},
+        { nit: 2, nombre: 'SuperOccidente', telefono: '987654321', direccion: 'Avenida 456', email: 'nuevoprove@example.com'},
+        { nit: 2, nombre: 'SuperOccidente', telefono: '987654321', direccion: 'Avenida 456', email: 'nuevoprove@example.com'},
+    ]
+
+    const tbody = document.querySelector('#tbodyProveedores tbody')
+    
+    tbody.innerHTML = ''
+    
+    dataProveedores.forEach(proveedor => {
+        const fila = `
+            <tr>
+                <td>${proveedor.nit}</td>
+                <td>${proveedor.nombre}</td>
+                <td>${proveedor.telefono}</td>
+                <td>${proveedor.direccion}</td>
+                <td>${proveedor.email}</td>
+            </tr>
+        `
+
+        tbody.innerHTML += fila
+    })
+}
